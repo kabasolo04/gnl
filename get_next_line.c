8bc1c48f16da98184697c7549d14302168c94cab
Koldo Abasolo Iturregi <koldobikaabasolo@gmail.com>
2024-02-02 13:38:40 +0100

    BuenoBonitoBarato

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 19:35:45 by kabasolo          #+#    #+#             */
-/*   Updated: 2024/02/01 18:40:57 by kabasolo         ###   ########.fr       */
+/*   Updated: 2024/02/02 13:33:28 by kabasolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,21 @@ char	*get_next_line(int fd)
 	char		*line;
 	int			rbytes;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
 	rbytes = 1;
 	line = (char *)malloc(sizeof(char) * 1);
+	if (!line || fd < 0 || BUFFER_SIZE <= 0)
+		return (free(line), NULL);
 	line[0] = '\0';
 	line = ft_join(line, buffer);
-	while (ft_find_jump(line) < 0 && rbytes > 0)
+	while (ft_lfc(line, '\n') < 0 && rbytes > 0)
 	{
 		rbytes = read(fd, buffer, BUFFER_SIZE);
 		if (rbytes == -1)
-			return (free(line), ft_bzero(buffer, ft_strlen(buffer)), NULL);
+			return (free(line), ft_bzero(buffer, ft_lfc(buffer, '\0')), NULL);
 		buffer[rbytes] = '\0';
 		line = ft_join(line, buffer);
 	}
 	if (line[0] == '\0')
 		return (free(line), NULL);
-	ft_cut_buffer(buffer);
-	return (line);
+	return (ft_cut_buffer(buffer), line);
 }
